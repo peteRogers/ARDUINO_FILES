0336115ecb9b8f918678afcaba43d477402cc3b0
Peter Rogers <p.rogers@gold.ac.uk>
2025-01-16 13:23:40 +0000

    Update FM_Receiver.ino

--- FM_Radio/FM_Receiver/FM_Receiver.ino
+++ FM_Radio/FM_Receiver/FM_Receiver.ino
@@ -1,32 +1,40 @@
 #include <RDA5807.h>
 RDA5807 rx; 
-long rds_elapsed = millis();
-long status_elapsed = millis();
+float freq = 0.0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(4, INPUT_PULLUP); // Arduino pin 4 - Seek station down
-  pinMode(5, INPUT_PULLUP); // Arduino pin 5 - Seek station up
+  pinMode(7, INPUT_PULLUP); // Arduino pin 7 - Seek station down
+  pinMode(8, INPUT_PULLUP); // Arduino pin 8 - Seek station up
   rx.setup(); // Starts the receiver with default parameters
   rx.setLnaPortSel(3);  // Trying improve sensitivity.
   rx.setAFC(true);      // Sets Automatic Frequency Control
   rx.setFmDeemphasis(1); //sorts for Europe broadcasting
-  rx.setFrequency(8700); // Tunes in 103.9 MHz  - Switch to your local favorite station
+  rx.setFrequency(9730); // Tunes in 103.9 MHz  - Switch to your local favorite station
   rx.setVolume(7);
-  // RDS setup
-  rx.setRDS(true);
-  rx.setRdsFifo(true);
-  
+
 }
 void loop() {
-  if (digitalRead(4) == LOW) rx.seek(RDA_SEEK_WRAP,RDA_SEEK_DOWN);
-  if (digitalRead(5) == LOW) rx.seek(RDA_SEEK_WRAP,RDA_SEEK_UP);
-
-   
-    
-  
+  // if (digitalRead(7) == LOW) {
+  //   rx.seek(RDA_SEEK_WRAP,RDA_SEEK_DOWN, showStatus);
+  // }
+  // if (digitalRead(8) == LOW) {
+  //   rx.seek(RDA_SEEK_WRAP,RDA_SEEK_UP, showStatus);
+  // }
+  freq = rx.getFrequency()/100.0;
+  Serial.println(freq);
   delay(200);
-  if (Serial.available() > 0) {
+ 
+}
+
+void showStatus() {
+  char aux[80];
+  sprintf(aux, "\nYou are tuned on %u MHz | RSSI: %3.3u dbUv | Vol: %2.2u | Stereo: %s\n", rx.getFrequency(), rx.getRssi(), rx.getVolume(), (rx.isStereo()) ? "Yes" : "No");
+  Serial.print(aux);
+}
+
+/*
+ if (Serial.available() > 0) {
     char key = Serial.read();
     switch (key) {
       case '+':
@@ -49,16 +57,9 @@ void loop() {
       case 's':
         rx.seek(RDA_SEEK_WRAP, RDA_SEEK_DOWN, showStatus);
         break;
-    
       default:
         break;
     }
   }
-}
-
-void showStatus() {
-  char aux[80];
-  sprintf(aux, "\nYou are tuned on %u MHz | RSSI: %3.3u dbUv | Vol: %2.2u | Stereo: %s\n", rx.getFrequency(), rx.getRssi(), rx.getVolume(), (rx.isStereo()) ? "Yes" : "No");
-  Serial.print(aux);
-  status_elapsed = millis();
-}
+  */
+  
