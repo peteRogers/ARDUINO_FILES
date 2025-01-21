35d95da89077d8e6a70b4d75a28061468adc722a
Peter Rogers <p.rogers@gold.ac.uk>
2025-01-21 09:30:06 +0000

    Update FM_Receiver.ino

--- FM_Radio/FM_Receiver/FM_Receiver.ino
+++ FM_Radio/FM_Receiver/FM_Receiver.ino
@@ -1,29 +1,39 @@
 #include <RDA5807.h>
+#include <Bounce2.h>
+Bounce buttonUp = Bounce();
+Bounce buttonDown = Bounce();
 RDA5807 rx; 
 float freq = 0.0;
 
+
 void setup() {
   Serial.begin(9600);
-  pinMode(7, INPUT_PULLUP); // Arduino pin 7 - Seek station down
-  pinMode(8, INPUT_PULLUP); // Arduino pin 8 - Seek station up
+  //buttons
+  buttonUp.attach(7, INPUT_PULLUP); // Arduino pin 7 - Seek station up
+  buttonDown.attach(8, INPUT_PULLUP); // Arduino pin 8 - Seek station down
+  buttonUp.interval(20);
+  buttonDown.interval(20);
+  //radio setup
   rx.setup(); // Starts the receiver with default parameters
   rx.setLnaPortSel(3);  // Trying improve sensitivity.
   rx.setAFC(true);      // Sets Automatic Frequency Control
   rx.setFmDeemphasis(1); //sorts for Europe broadcasting
   rx.setFrequency(9730); // Tunes in 103.9 MHz  - Switch to your local favorite station
-  rx.setVolume(7);
+  rx.setVolume(4);
 
 }
 void loop() {
-  // if (digitalRead(7) == LOW) {
+  // buttonUp.update();
+  // buttonDown.update();
+  // if (buttonUp.fell()) {
   //   rx.seek(RDA_SEEK_WRAP,RDA_SEEK_DOWN, showStatus);
   // }
-  // if (digitalRead(8) == LOW) {
+  // if (buttonDown.fell()) {
   //   rx.seek(RDA_SEEK_WRAP,RDA_SEEK_UP, showStatus);
   // }
   freq = rx.getFrequency()/100.0;
   Serial.println(freq);
-  delay(200);
+  delay(50);
  
 }
 
