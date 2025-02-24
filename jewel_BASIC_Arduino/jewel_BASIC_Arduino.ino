34e16a1a3faaa25edc24bde85eab4176a56156ca
Peter Rogers <p.rogers@gold.ac.uk>
2025-02-24 09:45:58 +0000

    Update jewel_BASIC_Arduino.ino

--- jewel_BASIC_Arduino/jewel_BASIC_Arduino.ino
+++ jewel_BASIC_Arduino/jewel_BASIC_Arduino.ino
@@ -11,3 +11,19 @@ void loop() {
   jewel.setPixelColor(0, 0, 0, 0, 255);
   jewel.show();
 }
+
+
+
+//function to get color from one number
+uint32_t Spectrum(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return jewel.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return jewel.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return jewel.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
